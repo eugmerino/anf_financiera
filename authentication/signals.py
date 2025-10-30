7b09a7da159845b49ea394e10c69560afe6dc64c
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-10-30 14:36:53 -0600

    [UPDATE] singal and settings

--- authentication/signals.py
+++ authentication/signals.py
@@ -4,18 +4,18 @@ from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 
 @receiver(post_migrate)
-def create_default_groups(sender, **kwargs):
+def create_default_groups_and_superuser(sender, **kwargs):
     if sender.name == 'authentication':
         groups = ['FINANCIERA', 'ACTIVO']
         for name in groups:
             Group.objects.get_or_create(name=name)
 
-User = get_user_model()
+    User = get_user_model()
 
-if not User.objects.filter(username='developer').exists():
-        user = User.objects.create_superuser(
-            username='developer',
-            email='developer@example.com',
-            password='default'
-        )
-        print('✅ Superusuario "developer" creado.')
+    if not User.objects.filter(username='developer').exists():
+            user = User.objects.create_superuser(
+                username='developer',
+                email='developer@example.com',
+                password='default'
+            )
+            print('✅ Superusuario "developer" creado.')
